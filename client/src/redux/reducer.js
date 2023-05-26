b23edcc4216a2f52a17d603ba150c78e0cde0029
Lautaro Barrios <barrios.g.lautaro@gmail.com>
2023-05-26 12:51:54 -0300

    Details

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -10,79 +10,83 @@ const initialState = {
 const reducer = (state = initialState, { type, payload }) => {
     switch (type) {
       case ALL_VIDEOGAMES:
-          return {
-              ...state,
-              allVideogames: payload,
-              videogamesDisplay: payload,
-          };
+        return {
+          ...state,
+          allVideogames: payload,
+          videogamesDisplay: payload,
+        };
+
       case VIDEOGAME_DETAILS:
-          return {
-              ...state,
-              videogameDetails: payload,
-          };
+        return {
+            ...state,
+            videogameDetails: payload,
+        };
+
       case VIDEOGAME_BY_NAME:
-          return {
-              ...state,
-              allVideogames: payload,
-              videogamesDisplay: payload,
-          };
+        return {
+          ...state,
+          allVideogames: payload,
+          videogamesDisplay: payload,
+        };
   
       case FILTER:
-          const currentFilters = payload;
-          let filteredGames = state.allVideogames;
+        const currentFilters = payload;
+        let filteredGames = state.allVideogames;
 
-      if (currentFilters.rating === "asc") {
-        filteredGames = [...filteredGames].sort((a, b) => a.rating - b.rating);
-      } else if (currentFilters.rating === "desc") {
-        filteredGames = [...filteredGames].sort((a, b) => b.rating - a.rating);
-      }
+        if (currentFilters.rating === "asc") {
+          filteredGames = [...filteredGames].sort((a, b) => a.rating - b.rating);
+        } else if (currentFilters.rating === "desc") {
+          filteredGames = [...filteredGames].sort((a, b) => b.rating - a.rating);
+        }
 
-      if (currentFilters.alphabetic === "asc") {
-        filteredGames = [...filteredGames].sort((gameA, gameB) => {
-          const nameA = gameA.name.toLowerCase();
-          const nameB = gameB.name.toLowerCase();
-          if (nameA < nameB) return -1;
-          if (nameA > nameB) return 1;
-          return 0;
-        });
-      } else if (currentFilters.alphabetic === "desc") {
-        filteredGames = [...filteredGames].sort((gameA, gameB) => {
-          const nameA = gameA.name.toLowerCase();
-          const nameB = gameB.name.toLowerCase();
-          if (nameA < nameB) return 1;
-          if (nameA > nameB) return -1;
-          return 0;
-        });
-      }
+        if (currentFilters.alphabetic === "asc") {
+          filteredGames = [...filteredGames].sort((gameA, gameB) => {
+            const nameA = gameA.name.toLowerCase();
+            const nameB = gameB.name.toLowerCase();
+            if (nameA < nameB) return -1;
+            if (nameA > nameB) return 1;
+            return 0;
+          });
+        } else if (currentFilters.alphabetic === "desc") {
+          filteredGames = [...filteredGames].sort((gameA, gameB) => {
+            const nameA = gameA.name.toLowerCase();
+            const nameB = gameB.name.toLowerCase();
+            if (nameA < nameB) return 1;
+            if (nameA > nameB) return -1;
+            return 0;
+          });
+        }
 
-      if (currentFilters.genre !== "none") {
-        filteredGames = filteredGames.filter((game) =>
-          game.genres.some((genre) => genre.name === currentFilters.genre)
-        );
-      }
+        if (currentFilters.genre !== "none") {
+          filteredGames = filteredGames.filter((game) =>
+            game.genres.some((genre) => genre.name === currentFilters.genre)
+          );
+        }
 
-      if (currentFilters.origin !== "none") {
-        filteredGames = filteredGames.filter(
-          (game) => game.origin === currentFilters.origin
-        );
-      }
-      return {
-        ...state,
-        videogamesDisplay: filteredGames,
-      };
+        if (currentFilters.origin !== "none") {
+          filteredGames = filteredGames.filter(
+            (game) => game.origin === currentFilters.origin
+          );
+        }
+        return {
+          ...state,
+          videogamesDisplay: filteredGames,
+        };
 
       case GENRES:
-          return {
-          ...state,
-          genres: payload,
-          };
+        return {
+        ...state,
+        genres: payload,
+        };
+
       case NEW_VIDEOGAME:
-          return {
-          ...state,
-          allVideogames: [...state.allVideogames, payload],
-          };
+        return {
+        ...state,
+        allVideogames: [...state.allVideogames, payload],
+        };
+
       default:
-          return state;
+        return state;
     }
 };
 
